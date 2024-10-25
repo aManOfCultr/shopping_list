11f1ed676ca9d8c5ec6c5691ac22608a5a85c77f
aManOfCultr <amancloud882@gmail.com>
2024-10-25 21:43:45 +0530

    create full functioning ui of the app

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,30 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/models/grocery_item.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-import 'package:shopping_list/data/dummy_items.dart';
-
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(
+        builder: (ctx) => NewItem(),
+      ),
+    );
+
+    if (newItem == null) {
+      return;
+    } else {
+      setState(() {
+        _groceryItems.add(newItem);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
       body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (ctx, index) => Card(
-
-          child: ListTile(
-            title: Text(groceryItems[index].name),
-            leading: Container(
-              width: 24,
-              height: 24,
-              color: groceryItems[index].category.color,
-            ),
-            trailing: Text(
-              groceryItems[index].quantity.toString(),
-            ),
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) => ListTile(
+          title: Text(_groceryItems[index].name),
+          leading: Container(
+            width: 24,
+            height: 24,
+            color: _groceryItems[index].category.color,
+          ),
+          trailing: Text(
+            _groceryItems[index].quantity.toString(),
           ),
         ),
       ),
